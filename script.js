76c63cf834fa7425c705f65c9348128cc1319515
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2022-12-29 11:37:07 -0500

    update search bar feature, fix deletebutton bug

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 //global variables
 const meals = document.getElementById('meals');
-//const defaultFav = ['52823', '53038', '52775'];
+const storageArray = getIdMealsFromLS();
+const searchTerm = document.getElementById('search-term');
+const searchBtn = document.getElementById('search');
+searchBtn.addEventListener('click', async () => {
+  const searched = searchTerm.value;
+  const searchResult = await getMealsBySearch(searched);
+  showSearchResult(searchResult);
+});
+
+
 
 // Execution
 
-//localStorage.setItem('IdMeals', JSON.stringify(defaultFav));
 getRandomMeal();
+setInterval(reload, 20000);
 fetchFromLS();
 
 
+
 // function declarations
+function reload() {
+  meals.innerHTML = '';
+  getRandomMeal();
+}
+
 
 // --> Random meals section
 async function getRandomMeal() {
@@ -65,9 +80,8 @@ function addIdMealToLS(IdMeal) {
 
 function removeIdMealFromLS(IdMeal) {
   const IdMeals = getIdMealsFromLS();
-  localStorage.setItem('IdMeals', JSON.stringify(IdMeals.filter((id) => {
-    id !== IdMeal;
-  })));
+  const newArray = IdMeals.filter((element) => element !== IdMeal);
+  localStorage.setItem('IdMeals', JSON.stringify(newArray));
 }
 
 
@@ -81,9 +95,8 @@ async function getMealById(id) {
 
 async function fetchFromLS() {
   const IdMeals = getIdMealsFromLS();
-  for (let IdMeal of IdMeals) {
-    let favMeal = await getMealById(IdMeal);
-    console.log(favMeal);
+  for (let i = 0; i < IdMeals.length; i++) {
+    let favMeal = await getMealById(IdMeals[i]);
     addMealToFav(favMeal);
   }
 }
@@ -91,8 +104,23 @@ async function fetchFromLS() {
 function addMealToFav(favMeal) {
   const favList = document.getElementById('fav-list');
   const newListItem = document.createElement('li');
-  newListItem.innerHTML = `<a href="${favMeal.strYoutube}" target="_blank"><img src="${favMeal.strMealThumb}" alt="${favMeal.strMeal}"></a> <span>${favMeal.strMeal.slice(0, 10).toUpperCase()}</span>
-  <button class="trash" id="${favMeal.idMeal}"><i class="fa-solid fa-square-minus"></i></button>`;
+  newListItem.innerHTML = `
+  <a href="${favMeal.strYoutube}" target="_blank">
+    <img src="${favMeal.strMealThumb}" alt="${favMeal.strMeal}">
+  </a> 
+  <span>
+    ${favMeal.strMeal.slice(0, 10).toUpperCase()}
+  </span>
+  <button class="clear">
+    <i class="fa-solid fa-square-minus"></i>
+  </button>`;
+
+  const btn = newListItem.querySelector('.clear');
+  btn.addEventListener('click', () => {
+    removeIdMealFromLS(favMeal.idMeal);
+    document.getElementById('fav-list').innerHTML = '';
+    fetchFromLS();
+  });
 
   favList.appendChild(newListItem);
 }
@@ -101,7 +129,32 @@ function addMealToFav(favMeal) {
 
 // --> search bar section
 async function getMealsBySearch(term) {
-  const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+  const searchResponse = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+
+  const responseData = await searchResponse.json();
+  const meals = responseData.meals;
+  console.log(meals)
+  return meals;
 }
+
+function showSearchResult(searchResult) {
+  const randomContainer = document.getElementById('meals');
+  randomContainer.innerHTML = '';
+  randomContainer.innerHTML = `
+  <div class="meal-header">
+    ${`<span class="random">Search Result</span>`}
+    <a href="${searchResult[0].strSource}" target="_blank">
+      <img src="${searchResult[0].strMealThumb}" alt="${searchResult[0].strMeal}">
+    </a>
+  </div>
+  <div class="meal-body">
+    <h4>${searchResult[0].strMeal}</h4>
+    <button class="fav-btn">
+      <i class="fa fa-heart"></i>
+    </button>
+  </div>`;
+}
+
+
 
 
